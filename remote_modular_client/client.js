b5343b5f309f8b74d7049b185949d8fa340313d8
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-04-02 12:10:50 +0200

    add osc to client

--- remote_modular_client/client.js
+++ remote_modular_client/client.js
@@ -1,14 +1,37 @@
 var io = require('socket.io-client');
+var osc = require("osc");
 
-var socket = io("http://localhost:3000");
+const SC_PORT = process.env.SC_PORT || 57120
+const SC_HOST = process.env.SC_HOST || "localhost"
+const CLIENT_PORT = process.env.CLIENT_PORT || 57220
+const BACKEND_ADDRESS = process.env.BACKEND_ADDRESS || "http://localhost:3000"
 
-socket.on('connect', function () { console.log("socket connected"); });
-socket.emit('private message', { user: 'me', msg: 'whazzzup?' });
+console.log("Start client")
+console.log("Backend address: " + BACKEND_ADDRESS)
+console.log("SuperCollider address: " + SC_HOST + ":" + SC_PORT)
+console.log("Client port: " + CLIENT_PORT)
+
+const socket = io(BACKEND_ADDRESS);
+
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: CLIENT_PORT,
+    remoteAddress: SC_HOST,
+    remotePort: SC_PORT,
+});
+udpPort.open();
+
+
+socket.on('connect', function () {
+    console.log("socket connected");
+});
 
 socket.on("slider change", (msg) => {
-    console.log(msg);
+    console.log("Received message " + msg);
+    udpPort.send({
+        address: "/hello/supercollider",
+        args: msg
+    });
 });
 
 console.log("Started client")
-
-console.log(socket);
